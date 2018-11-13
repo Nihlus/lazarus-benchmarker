737b12f55c4e5ff814c7c6adefd65a59846a4eed
DMP9 <dylanperks23@gmail.com>
2018-11-13 20:47:27 +0000

    this ain't it chief

--- Lazarus.Benchmarker/Program.cs
+++ Lazarus.Benchmarker/Program.cs
@@ -23,11 +23,12 @@ namespace Lazarus.Benchmarker
 
         #region benchmark
 
-        [SimpleJob]
+        [CoreJob,ClrJob,MonoJob()]
         [RankColumn]
-        public class LazarusBenchmarker
+        public class InvertMatrixByPtrBenchmark
         {
             private ILazarus _lazarus;
+            private Matrix2 _matrix;
 
             [Params("ADL, calli", "ADL, delegates", "Rewritten")]
             public string Method;
@@ -35,6 +36,8 @@ namespace Lazarus.Benchmarker
             [GlobalSetup]
             public void Setup()
             {
+                _matrix = new Matrix2()
+                    {Row0 = new Vector2() {X = 1f, Y = 0f}, Row1 = new Vector2() {X = 10f, Y = 5f}};
                 if (!File.Exists("lz.dll"))
                 using (var stream = Assembly.GetAssembly(typeof(ILazarus))
                     .GetManifestResourceStream("Lazarus.ADL." + (Environment.Is64BitProcess ? "lz64" : "lz32") +
@@ -64,31 +67,71 @@ namespace Lazarus.Benchmarker
             [Benchmark]
             public void InvertMatrixByPtr()
             {
-                    var matrix = new Matrix2()
-                        {Row0 = new Vector2() {X = 1f, Y = 0f}, Row1 = new Vector2() {X = 10f, Y = 5f}};
                     if (Method == "Rewritten")
-                        RW.Lazarus.InvertMatrixByPtr(ref matrix);
+                        RW.Lazarus.InvertMatrixByPtr(ref _matrix);
                     else
-                        _lazarus.InvertMatrixByPtr(ref matrix);
+                        _lazarus.InvertMatrixByPtr(ref _matrix);
+            }
+        }
+
+        
+        [CoreJob,CoreRtJob,ClrJob,MonoJob()]
+        [RankColumn]
+        public class InvertMatrixByValueBenchmark
+        {
+            private ILazarus _lazarus;
+            private Matrix2 _matrix;
+
+            [Params("ADL, calli", "ADL, delegates", "Rewritten")]
+            public string Method;
+
+            [GlobalSetup]
+            public void Setup()
+            {
+                _matrix = new Matrix2()
+                    {Row0 = new Vector2() {X = 1f, Y = 0f}, Row1 = new Vector2() {X = 10f, Y = 5f}};
+                ;
+                if (!File.Exists("lz.dll"))
+                using (var stream = Assembly.GetAssembly(typeof(ILazarus))
+                    .GetManifestResourceStream("Lazarus.ADL." + (Environment.Is64BitProcess ? "lz64" : "lz32") +
+                                               ".dll"))
+                {
+                    using (var output = File.OpenWrite("lz.dll"))
+                    {
+                        stream.CopyTo(output);
+                        output.Flush();
+                    }
+                }
+
+                if (Method == "ADL, delegates")
+                    _lazarus = NativeLibraryBuilder.Default.ActivateInterface<ILazarus>("lz.dll");
+                else if (Method == "ADL, calli")
+                    _lazarus = new NativeLibraryBuilder(ImplementationOptions.UseIndirectCalls |
+                                                        ImplementationOptions.EnableDllMapSupport |
+                                                        ImplementationOptions.EnableOptimizations |
+                                                        ImplementationOptions.SuppressSecurity)
+                        .ActivateInterface<ILazarus>("lz.dll");
+                else if (Method == "Rewritten")
+                {
+                    RW.Lazarus.LoadEntryPoints();
+                }
             }
 
             [Benchmark]
             public void InvertMatrixByValue()
             {
-                    var matrix = new Matrix2()
-                        {Row0 = new Vector2() {X = 1f, Y = 0f}, Row1 = new Vector2() {X = 10f, Y = 5f}};
                     if (Method == "Rewritten")
-                        RW.Lazarus.InvertMatrixByValue(matrix);
+                        RW.Lazarus.InvertMatrixByValue(_matrix);
                     else
-                        _lazarus.InvertMatrixByValue(matrix);
+                        _lazarus.InvertMatrixByValue(_matrix);
             }
         }
-
         #endregion
 
         static void Main(string[] args)
         {
-            BenchmarkRunner.Run<LazarusBenchmarker>(DefaultConfig.Instance.KeepBenchmarkFiles());
+            BenchmarkRunner.Run<InvertMatrixByPtrBenchmark>();
+            BenchmarkRunner.Run<InvertMatrixByValueBenchmark>();
         }
     }
 }
